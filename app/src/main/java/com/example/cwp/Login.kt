86f6c60ff2099121d29b846002a77e053f05143c
Heorhii <heorh.popov@gmail.com>
2023-09-27 13:59:01 +0300

    Created fragments for login and registration. Added navigation between these fragments. Created a draft design of login page.

---
+++ app/src/main/java/com/example/cwp/Login.kt
@@ -0,0 +1,16 @@
+package com.example.cwp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Login.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Login : Fragment(R.layout.fragment_login) {
+
+}
